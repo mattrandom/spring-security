66781e62667b68879e78201a3f44db6c1d18e09d
mattrandom <44298255+mattrandom@users.noreply.github.com>
2022-02-13 15:25:51 +0100

    Adding custom configuration of Spring Security and testing various scenario for particular endpoints

---
+++ 1-custom configuration/src/main/java/io/mattrandom/configuration/SecurityConfig.java
@@ -0,0 +1,64 @@
+package io.mattrandom.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * /myAccount - Secured
+     * /myBalance - Secured
+     * /myLoans - Secured
+     * /myCards - Secured
+     * /notices - Not Secured
+     * /contact - Not Secured
+     */
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /**
+         * Default configurations which will secure all the requests
+         */
+
+        /*
+         * http .authorizeRequests() .anyRequest().authenticated() .and()
+         * .formLogin().and() .httpBasic();
+         */
+
+
+        /**
+         * Custom configurations as per our requirement
+         */
+        http
+                .authorizeRequests()
+                    .antMatchers("/myAccount/**").authenticated()
+                    .antMatchers("/myBalance/**").authenticated()
+                    .antMatchers("/myLoans/**").authenticated()
+                    .antMatchers("/myCards/**").authenticated()
+                    .anyRequest().permitAll()
+                    .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+
+        /**
+         * Configuration to deny all the requests
+         */
+
+        /*
+         * http .authorizeRequests() .anyRequest().denyAll() .and() .formLogin().and()
+         * .httpBasic();
+         */
+
+        /**
+         * Configuration to permit all the requests
+         */
+
+//        http .authorizeRequests() .anyRequest().permitAll().and() .formLogin().and()
+//                .httpBasic();
+    }
+}
