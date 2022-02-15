d27174b763d52f837e16dbafa2a3db55c9f2869b
mattrandom <44298255+mattrandom@users.noreply.github.com>
2022-02-15 17:42:10 +0100

    2-configuring users using InMemoryUserDetailsManager

--- 2-user management/src/main/java/io/mattrandom/configuration/SecurityConfig.java
+++ 2-user management/src/main/java/io/mattrandom/configuration/SecurityConfig.java
@@ -1,11 +1,18 @@
 package io.mattrandom.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -34,12 +41,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("admin").password("pass").authorities("ADMIN")
+//                .and()
+//                .withUser("user").password("pass").authorities("READ")
+//                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    }
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("pass").authorities("ADMIN")
-                .and()
-                .withUser("user").password("pass").authorities("READ")
-                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+        UserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.withUsername("admin").password(passwordEncoder().encode("pass")).authorities("ADMIN").build();
+        UserDetails user2 = User.withUsername("user").password(passwordEncoder().encode("pass")).authorities("USER").build();
+
+        userDetailsService.createUser(user);
+        userDetailsService.createUser(user2);
+
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
